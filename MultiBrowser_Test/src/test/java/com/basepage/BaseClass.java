79ab5152150158f8b32847d3a3eb62e060ff360e
huquemir <huque.mir@gmail.com>
2022-09-29 15:57:44 -0400

    second commit

---
+++ MultiBrowser_Test/src/test/java/com/basepage/BaseClass.java
@@ -0,0 +1,21 @@
+package com.basepage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.variables.ConfigProperties;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	
+	
+	public static void setUp() {
+		
+
+		
+	}
+
+}
---
+++ MultiBrowser_Test/src/test/java/com/hooks/Hook.java
@@ -0,0 +1,19 @@
+package com.hooks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.basepage.BaseClass;
+import com.variables.ConfigProperties;
+
+import cucumber.api.java.Before;
+
+
+public class Hook {
+	
+	
+
+	
+
+}
